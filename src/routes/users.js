a458dafb38caeddc7158032b3241c9872007a348
mss <2419465497@qq.com>
2019-02-27 15:49:39 +0800

    后台完成

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var userAPI = require('./users/')
 
 /* GET users listing. */
-router.get('/', userAPI.queryuser);
+router.get('/api/adduser', userAPI.queryuser);
+
 router.get('/', userAPI.adduser);
 
 module.exports = router;
--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,5 +1,23 @@
+var Mongo = require('mongodb-curd');
+var batabaseName = 'lemon';
+var collcationName = 'user'
+
+
 function queryuser(req, res, next) {
-    console.log('15246')
+    Mongo.find(batabaseName, collcationName, {}, function(result) {
+        if (!result) {
+            res.send({
+                code: 0,
+                mes: "error"
+            })
+        } else {
+            res.send({
+                code: 1,
+                mes: "success",
+                data: result
+            })
+        }
+    })
 }
 
 function adduser(req, res, next) {
